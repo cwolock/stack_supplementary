307adb5d9972ed6092d72d69f9e57139c7b1d007
cwolock <cwolock@uw.edu>
2023-10-18 23:37:57 -0400

    updated rate sims to also vary approx grid within each

--- sims/rates/do_one.R
+++ sims/rates/do_one.R
@@ -21,7 +21,7 @@ do_one <- function(n_train, n_test=1000, rate, dgp){
                                probs = c(0.5, 0.75, 0.9)),
                       digits = 0)
   # benchmarks
-  # approx_times <- sort(unique(train$Y[train$Delta == 1]))
+  approx_times <- sort(unique(train$Y[train$Delta == 1]))
   benchmark_times <- seq(0.1, 100, by = 0.1)
 
   # calculate true survival function values
@@ -42,8 +42,8 @@ do_one <- function(n_train, n_test=1000, rate, dgp){
   SL.library <- c("SL.mean", "SL.glm.interaction", "SL.earth",
                   "SL.gam", "SL.ranger", xgb_grid$names)
   start_time <- Sys.time()
+  approx_rates <- c("0.25", "0.333", "0.5", "0.667", "0.75", "1")
   if (rate == "1/4"){
-    approx_times <- quantile(train$Y, probs = seq(0, 1, length.out = n_train^(1/4)))
     out <- survML::stackG(time = train$Y,
                           event = train$Delta,
                           X = train[,1:dimension],
@@ -55,9 +55,7 @@ do_one <- function(n_train, n_test=1000, rate, dgp){
                           surv_form = "exp",
                           SL_control = list(SL.library = SL.library,
                                             V = 5))
-    est_df <- out$S_T_preds
   } else if (rate == "1/3"){ # global stacking, all times grid
-    approx_times <- quantile(train$Y, probs = seq(0, 1, length.out = n_train^(1/3)))
     out <- survML::stackG(time = train$Y,
                           event = train$Delta,
                           X = train[,1:dimension],
@@ -69,9 +67,7 @@ do_one <- function(n_train, n_test=1000, rate, dgp){
                           surv_form = "exp",
                           SL_control = list(SL.library = SL.library,
                                             V = 5))
-    est_df <- out$S_T_preds
   } else if (rate == "1/2"){ # global stacking 0.025 grid
-    approx_times <- quantile(train$Y, probs = seq(0, 1, length.out = n_train^(1/2)))
     out <- survML::stackG(time = train$Y,
                           event = train$Delta,
                           X = train[,1:dimension],
@@ -83,9 +79,7 @@ do_one <- function(n_train, n_test=1000, rate, dgp){
                           surv_form = "exp",
                           SL_control = list(SL.library = SL.library,
                                             V = 5))
-    est_df <- out$S_T_preds
   } else if (rate == "2/3"){ # global stacking, 0.1 grid
-    approx_times <- quantile(train$Y, probs = seq(0, 1, length.out = n_train^(2/3)))
     out <- survML::stackG(time = train$Y,
                           event = train$Delta,
                           X = train[,1:dimension],
@@ -97,9 +91,7 @@ do_one <- function(n_train, n_test=1000, rate, dgp){
                           surv_form = "exp",
                           SL_control = list(SL.library = SL.library,
                                             V = 5))
-    est_df <- out$S_T_preds
   } else if (rate == "3/4"){ # global stacking, 0.1 grid
-    approx_times <- quantile(train$Y, probs = seq(0, 1, length.out = n_train^(3/4)))
     out <- survML::stackG(time = train$Y,
                           event = train$Delta,
                           X = train[,1:dimension],
@@ -111,9 +103,7 @@ do_one <- function(n_train, n_test=1000, rate, dgp){
                           surv_form = "exp",
                           SL_control = list(SL.library = SL.library,
                                             V = 5))
-    est_df <- out$S_T_preds
   } else if (rate == "1"){ # global stacking, 0.1 grid
-    approx_times <- quantile(train$Y, probs = seq(0, 1, length.out = n_train))
     out <- survML::stackG(time = train$Y,
                           event = train$Delta,
                           X = train[,1:dimension],
@@ -125,22 +115,37 @@ do_one <- function(n_train, n_test=1000, rate, dgp){
                           surv_form = "exp",
                           SL_control = list(SL.library = SL.library,
                                             V = 5))
-    est_df <- out$S_T_preds
   }
-  end_time <- Sys.time()
+  for (i in 1:length(approx_rates)){
+    approx_rate_numeric <- as.numeric(approx_rates[i])
+    approx_times <- quantile(train$Y, probs = seq(0, 1, length.out = n_train^approx_rate_numeric))
+    preds <- predict(out,
+                     time_grid_approx = approx_times,
+                     newX = test[,1:dimension],
+                     newtimes = benchmark_times)
+    est_df <- preds$S_T_preds
 
-  ### wrap things up
-  squared_errors <- (est_df - true_df_uni)^2
-  MSE_uni <- mean(squared_errors)
-  landmark_indices <- which(round(benchmark_times, digits = 2) %in% theo_quant)
-  landmark_estimates <- est_df[, landmark_indices]
-  landmark_truth <- true_df_uni[, landmark_indices]
-  landmark_sq_error <- (landmark_estimates - landmark_truth)^2
-  landmark_MSE <- colSums(landmark_sq_error)/n_test
-  output <- data.frame(MSE_uni = MSE_uni,
-                       landmark_MSE_50 = landmark_MSE[1],
-                       landmark_MSE_75 = landmark_MSE[2],
-                       landmark_MSE_90 = landmark_MSE[3])
+    ### wrap things up
+    squared_errors <- (est_df - true_df_uni)^2
+    MSE_uni <- mean(squared_errors)
+    landmark_indices <- which(round(benchmark_times, digits = 2) %in% theo_quant)
+    landmark_estimates <- est_df[, landmark_indices]
+    landmark_truth <- true_df_uni[, landmark_indices]
+    landmark_sq_error <- (landmark_estimates - landmark_truth)^2
+    landmark_MSE <- colSums(landmark_sq_error)/n_test
+    output <- data.frame(MSE_uni = MSE_uni,
+                         landmark_MSE_50 = landmark_MSE[1],
+                         landmark_MSE_75 = landmark_MSE[2],
+                         landmark_MSE_90 = landmark_MSE[3])
+    output$approx_rate <- approx_rates[i]
+    if (i == 1){
+      pooled_output <- output
+    } else{
+      pooled_output <- rbind(pooled_output, output)
+    }
+  }
+  output <- pooled_output
+  end_time <- Sys.time()
   output$dgp <- rep(dgp, nrow(output))
   output$n_train <- rep(n_train, nrow(output))
   output$rate <- rep(rate, nrow(output))
